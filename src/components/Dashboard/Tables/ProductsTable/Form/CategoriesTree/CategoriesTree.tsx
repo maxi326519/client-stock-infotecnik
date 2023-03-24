b1477718e5a73facd9383a5787afd73e9bcb85d4
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2023-03-24 13:14:26 -0300

    Fix error

--- src/components/Dashboard/Tables/ProductsTable/Form/CategoriesTree/CategoriesTree.tsx
+++ src/components/Dashboard/Tables/ProductsTable/Form/CategoriesTree/CategoriesTree.tsx
@@ -211,7 +211,6 @@ const CategoriesTree = ({ categories, handleClose }: Props) => {
               <button
                 className="btn btn-success"
                 type="button"
-                disabled={selectedNode === "categorias"}
                 onClick={handleSuibmit}
               >
                 Guardar
